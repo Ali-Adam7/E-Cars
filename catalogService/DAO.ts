29ca948f14e0babaacc18782058ea826822536b2
Ali Adam <46465490+Ali-Adam7@users.noreply.github.com>
2023-10-31 22:09:12 -0400

    Catalog microservice API endpoints
    - Sql initial script
    - Implemented the CRUD operations for the catalog service
    - Supports get all cars, get car by id and get car by filters such as make, model, type, price, milage.
    - edit car by id
    - delete car by id
    - Error handling
    - added env variables

--- catalogService/DAO.ts
+++ catalogService/DAO.ts
@@ -1,29 +1,100 @@
-import { car } from "./model";
-const mysql = require('mysql2/promise');
+import { car, filter } from "./model";
+const mysql = require("mysql2/promise");
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+dotenv.config();
 
 export class DAO {
   connection: any;
-  createConnection = async(host: string, user: string, password: string, database: string) => {
-    this.connection =  await mysql.createConnection({
-        host: host,
-        user: user,
-        password: password,
-        database: database,
+  createConnection = async () => {
+    this.connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      connectionLimit: process.env.DB_CCONNECTION_LIMIT,
+      multipleStatements: true,
+    });
+
+    // initlize the sql db:
+
+    const dataSql = fs.readFileSync(path.join(__dirname, "/db/init.sql")).toString();
+    const query = await this.connection.query(dataSql);
+  };
+  getAllCars = async (): Promise<car[] | string> => {
+    try {
+      let [rows, fields] = await this.connection.execute("SELECT * FROM catalog.cars");
+      return rows;
+    } catch (error: any) {
+      return error.sqlMessage;
+    }
+  };
+
+  getByID = async (id: string): Promise<car | string | null> => {
+    try {
+      let [rows, fields] = await this.connection.execute(`SELECT * FROM catalog.cars WHERE id = ${id}`);
+      if (rows.length) return rows[0];
+      else return null;
+    } catch (error: any) {
+      return error.sqlMessage;
+    }
+  };
+
+  getByFilter = async (filters: filter): Promise<car[]> => {
+    try {
+      const entries = Object.entries(filters);
+      let query: string = `SELECT * FROM catalog.cars WHERE `;
+      entries.forEach((entry: [string, any]) => {
+        if (entry[0] === "price" || entry[0] === "milage") query += `${entry[0]} <= '${entry[1]}' AND `;
+        else query += `${entry[0]} = '${entry[1]}' AND `;
       });
-  }
-  getAllCars = async (): Promise<car[]> => {
-    let [rows, fields] = await this.connection.execute('SELECT * FROM catalog.cars');
-    return rows
-};
+      query = query.substring(0, query.length - 4);
+      let [rows, fields] = await this.connection.execute(query);
+      if (rows.length) return rows as car[];
+      return [];
+    } catch (error: any) {
+      return error.sqlMessage;
+    }
+  };
 
-getByID = async (id:string): Promise<car | null>=> {
-    let [rows, fields] = await this.connection.execute(`SELECT * FROM catalog.cars WHERE id = ${id}`);
-    if(rows.length)
-    return rows[0]
-    else return null
-};
+  addCar = async (car: car): Promise<string> => {
+    const { make, model, type, price, img, year, history, milage } = car;
+    try {
+      const q = await this.connection.execute(
+        `INSERT INTO catalog.cars (make, model,type,price,img,year,history,milage) VALUES ("${make}","${model}","${type}", ${price},"${img}", ${year},"${history}", ${milage})`
+      );
+      return q[0].affectedRows;
+    } catch (error: any) {
+      return error.sqlMessage;
+    }
+  };
+  editCar = async (car: car, id: string): Promise<string> => {
+    try {
+      const entries = Object.entries(car);
+      let q = `UPDATE catalog.cars SET `;
 
-
+      entries.forEach((entry) => {
+        if (entry[0] == "year" || entry[0] == "milage") q += `${entry[0]} = ${entry[1]}, `;
+        else q += `${entry[0]} = '${entry[1]}', `;
+      });
+      q = q.substring(0, q.length - 2);
+      q += ` WHERE id = ${id}`;
+      const query = await this.connection.execute(q);
+      return query[0].affectedRows;
+    } catch (error: any) {
+      return error;
+    }
+  };
+  deleteCar = async (id: number): Promise<string> => {
+    try {
+      const q = await this.connection.execute(`DELETE FROM catalog.cars WHERE id = ${id}`);
+      return q[0].affectedRows;
+    } catch (error: any) {
+      return error.sqlMessage;
+    }
+  };
   end = () => {
     this.connection.end();
   };
--- catalogService/services.ts
+++ catalogService/services.ts
@@ -3,32 +3,72 @@ import { car } from "./model";
 import { DAO } from "./DAO";
 
 const dao = new DAO();
-dao.createConnection("127.0.0.1", "root", "password", "catalog");
+dao.createConnection();
 
-// implement DAO API to talk to the DB and use them in the functions below to retreive data
-
-export const getAll: RequestHandler = async (req, res) => {
-  // TODO
-  const cars = await dao.getAllCars();
-  res.json(cars);
+export const getCars: RequestHandler = async (req, res) => {
+  try {
+    if (!Object.keys(req.query).length) {
+      const cars = await dao.getAllCars();
+      res.json(cars);
+    } else {
+      getByFilters(req, res, () => {});
+    }
+  } catch (error: any) {
+    res.send(error.sqlMessage);
+  }
 };
 
-export const getCarByID:RequestHandler = async (req, res) => {
-  // TODO
+export const getCarByID: RequestHandler = async (req, res) => {
   const id: string = req.params.id as string;
   const cars = await dao.getByID(id);
-  if (!cars) res.status(404).send({error:"Car not found"});
+  if (!cars) res.status(404).send("Car not found");
   else {
     res.json(cars);
   }
 };
 
-export const getByFilters: RequestHandler = (req, res) => {
-  // TODO
-  // Check req query for filters
+export const getByFilters: RequestHandler = async (req, res) => {
+  try {
+    const cars: car[] = await dao.getByFilter(req.query);
+    res.json(cars);
+  } catch (error: any) {
+    res.send(error.sqlMessage);
+  }
 };
 
-export const addItem: RequestHandler = (req, res) => {
-  // TODO
-  // Ensure only admins can add items
+export const addCar: RequestHandler = async (req, res) => {
+  const car: car = req.body as car;
+
+  try {
+    const query = await dao.addCar(car);
+    if (query === "1") res.send("Car added");
+    else res.json({ Result: query });
+  } catch (error: any) {
+    res.send(error.sqlMessage);
+  }
+};
+
+export const editCar: RequestHandler = async (req, res) => {
+  try {
+    const car: car = req.body as car;
+    const id = req.params.id;
+    const q = await dao.editCar(car, id);
+    if (q) res.send("Car edited");
+    else res.send("Car not edited");
+  } catch (error: any) {
+    res.send(error.sqlMessage);
+  }
+};
+
+export const deleteCar: RequestHandler = async (req, res) => {
+  try {
+    if (req.params.id) {
+      const id = parseInt(req.params.id?.toString());
+      const query = await dao.deleteCar(id);
+      if (query) res.send(query);
+      else res.send(`The car with id ${req.params.id} did not get deleted`);
+    }
+  } catch (error: any) {
+    res.send(error.sqlMessage);
+  }
 };
